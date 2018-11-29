27bb149119341590ba78c4d0b88348cd7b7791b1
sespesogil <42008709+sespesogil@users.noreply.github.com>
2018-11-29 14:11:13 -0500

    Create automat_chrom3D_colors.sh

---
+++ automat_chrom3D_colors.sh
@@ -0,0 +1,18 @@
+
+#!/bin/bash
+
+module load bedtools/2.24.0 python py_packages
+
+grep beadID $1 | awk -F' ' '{print $11}' | sed -e "s/[=:-]\+/\t/g" | sed -e "s/[_AB]\+//g" | sed 's/\"/\t/g' | awk -F'\t' '{print $3"\t"$4"\t"$5"\t"}' > bead.ids
+
+bedtools intersect -a bead.ids -b $2 -wa > temp
+
+rm bead.ids
+
+awk -F'\t' '{print $1":"$2"-"$3}' temp > coord.bed
+
+python make_diploid_gtrack.py coord.bed > color_beads.ids
+
+python color_beads.py $1 color_beads.ids $3 $4 > $1.ROI.cmm
+
+rm temp coord.bed color_beads.ids
